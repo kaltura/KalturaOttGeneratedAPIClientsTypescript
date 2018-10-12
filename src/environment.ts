8c718ef38ce15a8b34cf06f7a8cd2cafd0728065
Jenkins <jenkins@kaltura.com>
2018-10-12 23:21:25 +0000

    Auto-generated by Jenkins

--- src/environment.ts
+++ src/environment.ts
@@ -9,7 +9,7 @@ export interface Environment {
 
 export const environment: Environment = {
     request: {
-        apiVersion: '5.0.3.42001'
+        apiVersion: '5.0.3.42007'
     },
     response: {
         nestedResponse: true
