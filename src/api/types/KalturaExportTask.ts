38144fe3f92c9e49382e35db07241adcb8c51cc3
Jenkins <jenkins@kaltura.com>
2019-01-10 23:48:06 +0000

    Auto-generated by Jenkins

--- src/api/types/KalturaExportTask.ts
+++ src/api/types/KalturaExportTask.ts
@@ -3,7 +3,6 @@ import { KalturaObjectMetadata } from '../kaltura-object-base';
 import { KalturaTypesFactory } from '../kaltura-types-factory';
 import { KalturaExportDataType } from './KalturaExportDataType';
 import { KalturaExportType } from './KalturaExportType';
-import { KalturaIntegerValue } from './KalturaIntegerValue';
 import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
 
 export interface KalturaExportTaskArgs  extends KalturaObjectBaseArgs {
@@ -14,7 +13,6 @@ export interface KalturaExportTaskArgs  extends KalturaObjectBaseArgs {
 	exportType? : KalturaExportType;
 	frequency? : number;
 	notificationUrl? : string;
-	vodTypes? : KalturaIntegerValue[];
 	isActive? : boolean;
 }
 
@@ -29,13 +27,11 @@ export class KalturaExportTask extends KalturaObjectBase {
 	exportType : KalturaExportType;
 	frequency : number;
 	notificationUrl : string;
-	vodTypes : KalturaIntegerValue[];
 	isActive : boolean;
 
     constructor(data? : KalturaExportTaskArgs)
     {
         super(data);
-        if (typeof this.vodTypes === 'undefined') this.vodTypes = [];
     }
 
     protected _getMetadata() : KalturaObjectMetadata
@@ -53,7 +49,6 @@ export class KalturaExportTask extends KalturaObjectBase {
 				exportType : { type : 'es', subTypeConstructor : KalturaExportType, subType : 'KalturaExportType' },
 				frequency : { type : 'n' },
 				notificationUrl : { type : 's' },
-				vodTypes : { type : 'a', subTypeConstructor : KalturaIntegerValue, subType : 'KalturaIntegerValue' },
 				isActive : { type : 'b' }
             }
         );
