62e69135378e9f36120f8392e95e304289f4e6e8
Backend CI <ott.rnd.core@kaltura.com>
2019-07-01 07:16:48 +0000

    Auto-generated by Jenkins

---
+++ src/api/types/KalturaAssetIndexStatus.ts
@@ -0,0 +1,7 @@
+
+
+export enum KalturaAssetIndexStatus {
+    deleted = 'Deleted',
+	notupdated = 'NotUpdated',
+	ok = 'Ok'
+}
--- src/environment.ts
+++ src/environment.ts
@@ -12,7 +12,7 @@ export interface Environment {
 
 export const environment: Environment = {
     request: {
-        apiVersion: '5.2.3.28518',
+        apiVersion: '5.2.3.43080',
         avoidQueryString: true,
         fileFormatValue: 20
     },
