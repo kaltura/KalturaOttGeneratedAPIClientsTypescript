2676981d8e65f7b6c604a4a3ca39035a8126ce95
Backend CI <ott.rnd.core@kaltura.com>
2019-10-28 10:06:12 +0000

    Auto-generated by Jenkins

--- src/environment.ts
+++ src/environment.ts
@@ -12,7 +12,7 @@ export interface Environment {
 
 export const environment: Environment = {
     request: {
-        apiVersion: '5.2.6.6039',
+        apiVersion: '5.2.6.6040',
         avoidQueryString: true,
         fileFormatValue: 20
     },
