ac256b35a1ac14c765d0b4fa3c25dbb0d409d183
Backend CI <ott.rnd.core@kaltura.com>
2020-06-17 08:59:18 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs, build 66, branch master

--- src/api/types/CategoryTreeGetAction.ts
+++ src/api/types/CategoryTreeGetAction.ts
@@ -6,6 +6,7 @@ import { KalturaRequest, KalturaRequestArgs } from '../kaltura-request';
 
 export interface CategoryTreeGetActionArgs  extends KalturaRequestArgs {
     categoryItemId : number;
+	filter : boolean;
 }
 
 /**
@@ -21,6 +22,7 @@ export interface CategoryTreeGetActionArgs  extends KalturaRequestArgs {
 export class CategoryTreeGetAction extends KalturaRequest<KalturaCategoryTree> {
 
     categoryItemId : number;
+	filter : boolean;
 
     constructor(data : CategoryTreeGetActionArgs)
     {
@@ -35,7 +37,8 @@ export class CategoryTreeGetAction extends KalturaRequest<KalturaCategoryTree> {
             {
                 service : { type : 'c', default : 'categorytree' },
 				action : { type : 'c', default : 'get' },
-				categoryItemId : { type : 'n' }
+				categoryItemId : { type : 'n' },
+				filter : { type : 'b' }
             }
         );
         return result;
--- src/environment.ts
+++ src/environment.ts
@@ -12,7 +12,7 @@ export interface Environment {
 
 export const environment: Environment = {
     request: {
-        apiVersion: '5.3.5.28096',
+        apiVersion: '5.3.6.28126',
         avoidQueryString: true,
         fileFormatValue: 20
     },
