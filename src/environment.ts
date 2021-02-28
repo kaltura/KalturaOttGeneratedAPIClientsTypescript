436869f311e12bb317e71ac9262a4f4a816937c8
Backend CI <ott.rnd.core@kaltura.com>
2021-02-28 06:36:51 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/393, branch BEO-9453-epg-invalidation

--- src/environment.ts
+++ src/environment.ts
@@ -12,7 +12,7 @@ export interface Environment {
 
 export const environment: Environment = {
     request: {
-        apiVersion: '6.1.0.28923',
+        apiVersion: '6.1.0.28931',
         avoidQueryString: true,
         fileFormatValue: 20
     },
