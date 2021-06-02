2c45d48d42c478154f99329e6e75f509fc53dd81
Backend CI <ott.rnd.core@kaltura.com>
2021-06-02 11:16:27 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/481, branch nullableProperties

--- src/environment.ts
+++ src/environment.ts
@@ -12,7 +12,7 @@ export interface Environment {
 
 export const environment: Environment = {
     request: {
-        apiVersion: '6.4.0.29151',
+        apiVersion: '6.4.0.29155',
         avoidQueryString: true,
         fileFormatValue: 20
     },
