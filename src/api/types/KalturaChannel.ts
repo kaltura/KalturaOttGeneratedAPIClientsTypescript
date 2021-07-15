d63a1d0a11fed12b62598a76812a4453121c7c06
Backend CI <ott.rnd.core@kaltura.com>
2021-07-15 06:15:29 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/543, branch 670_BEO-10281_channel-virtual-asset

--- src/api/types/KalturaChannel.ts
+++ src/api/types/KalturaChannel.ts
@@ -36,6 +36,7 @@ export class KalturaChannel extends KalturaBaseChannel {
 	supportSegmentBasedOrdering : boolean;
 	assetUserRuleId : number;
 	metaData : { [key : string] : KalturaStringValue};
+	readonly virtualAssetId : number;
 
     constructor(data? : KalturaChannelArgs)
     {
@@ -64,7 +65,8 @@ export class KalturaChannel extends KalturaBaseChannel {
 				updateDate : { type : 'n', readOnly : true },
 				supportSegmentBasedOrdering : { type : 'b' },
 				assetUserRuleId : { type : 'n' },
-				metaData : { type : 'm', subTypeConstructor : KalturaStringValue, subType : 'KalturaStringValue' }
+				metaData : { type : 'm', subTypeConstructor : KalturaStringValue, subType : 'KalturaStringValue' },
+				virtualAssetId : { type : 'n', readOnly : true }
             }
         );
         return result;
