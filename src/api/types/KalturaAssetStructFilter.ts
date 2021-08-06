31644c978b111330bb6ef619a1f6a723f0ffeeec
Backend CI <ott.rnd.core@kaltura.com>
2021-08-06 11:40:04 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/570, branch 671_BEO-10342_Subscriptions_Management

--- src/api/types/KalturaAssetStructFilter.ts
+++ src/api/types/KalturaAssetStructFilter.ts
@@ -1,12 +1,14 @@
 
 import { KalturaObjectMetadata } from '../kaltura-object-base';
 import { KalturaTypesFactory } from '../kaltura-types-factory';
+import { KalturaObjectVirtualAssetInfoType } from './KalturaObjectVirtualAssetInfoType';
 import { KalturaFilter, KalturaFilterArgs } from './KalturaFilter';
 
 export interface KalturaAssetStructFilterArgs  extends KalturaFilterArgs {
     idIn? : string;
 	metaIdEqual? : number;
 	isProtectedEqual? : boolean;
+	objectVirtualAssetInfoTypeEqual? : KalturaObjectVirtualAssetInfoType;
 }
 
 
@@ -15,6 +17,7 @@ export class KalturaAssetStructFilter extends KalturaFilter {
     idIn : string;
 	metaIdEqual : number;
 	isProtectedEqual : boolean;
+	objectVirtualAssetInfoTypeEqual : KalturaObjectVirtualAssetInfoType;
 
     constructor(data? : KalturaAssetStructFilterArgs)
     {
@@ -30,7 +33,8 @@ export class KalturaAssetStructFilter extends KalturaFilter {
                 objectType : { type : 'c', default : 'KalturaAssetStructFilter' },
 				idIn : { type : 's' },
 				metaIdEqual : { type : 'n' },
-				isProtectedEqual : { type : 'b' }
+				isProtectedEqual : { type : 'b' },
+				objectVirtualAssetInfoTypeEqual : { type : 'es', subTypeConstructor : KalturaObjectVirtualAssetInfoType, subType : 'KalturaObjectVirtualAssetInfoType' }
             }
         );
         return result;
