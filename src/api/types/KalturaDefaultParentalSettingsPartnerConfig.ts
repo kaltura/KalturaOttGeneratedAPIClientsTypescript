47a5c8d31c3f05771000c36b6a29cbf8ccd4ad8c
Backend CI <ott.rnd.core@kaltura.com>
2021-10-27 14:33:26 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/677, branch BEO-10875_parental_rule_default_partner_settings

--- src/api/types/KalturaDefaultParentalSettingsPartnerConfig.ts
+++ src/api/types/KalturaDefaultParentalSettingsPartnerConfig.ts
@@ -6,8 +6,8 @@ import { KalturaPartnerConfiguration, KalturaPartnerConfigurationArgs } from './
 export interface KalturaDefaultParentalSettingsPartnerConfigArgs  extends KalturaPartnerConfigurationArgs {
     defaultMoviesParentalRuleId? : number;
 	defaultTvSeriesParentalRuleId? : number;
-	defaultParentalPin? : number;
-	defaultPurchasePin? : number;
+	defaultParentalPin? : string;
+	defaultPurchasePin? : string;
 	defaultPurchaseSettings? : number;
 }
 
@@ -16,8 +16,8 @@ export class KalturaDefaultParentalSettingsPartnerConfig extends KalturaPartnerC
 
     defaultMoviesParentalRuleId : number;
 	defaultTvSeriesParentalRuleId : number;
-	defaultParentalPin : number;
-	defaultPurchasePin : number;
+	defaultParentalPin : string;
+	defaultPurchasePin : string;
 	defaultPurchaseSettings : number;
 
     constructor(data? : KalturaDefaultParentalSettingsPartnerConfigArgs)
@@ -34,8 +34,8 @@ export class KalturaDefaultParentalSettingsPartnerConfig extends KalturaPartnerC
                 objectType : { type : 'c', default : 'KalturaDefaultParentalSettingsPartnerConfig' },
 				defaultMoviesParentalRuleId : { type : 'n' },
 				defaultTvSeriesParentalRuleId : { type : 'n' },
-				defaultParentalPin : { type : 'n' },
-				defaultPurchasePin : { type : 'n' },
+				defaultParentalPin : { type : 's' },
+				defaultPurchasePin : { type : 's' },
 				defaultPurchaseSettings : { type : 'n' }
             }
         );
--- src/environment.ts
+++ src/environment.ts
@@ -12,7 +12,7 @@ export interface Environment {
 
 export const environment: Environment = {
     request: {
-        apiVersion: '6.8.0.29566',
+        apiVersion: '6.8.0.29576',
         avoidQueryString: true,
         fileFormatValue: 20
     },
