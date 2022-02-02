eda51730e066ac4d3a3d1af639feef47323d25dc
Backend CI <ott.rnd.core@kaltura.com>
2022-02-02 18:57:59 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/780, branch 710_BEO-11144_PAGO_New

---
+++ src/api/types/KalturaAssetOrderByType.ts
@@ -0,0 +1,14 @@
+
+
+export enum KalturaAssetOrderByType {
+    createDateAsc = 'CREATE_DATE_ASC',
+	createDateDesc = 'CREATE_DATE_DESC',
+	likesDesc = 'LIKES_DESC',
+	nameAsc = 'NAME_ASC',
+	nameDesc = 'NAME_DESC',
+	ratingsDesc = 'RATINGS_DESC',
+	relevancyDesc = 'RELEVANCY_DESC',
+	startDateAsc = 'START_DATE_ASC',
+	startDateDesc = 'START_DATE_DESC',
+	votesDesc = 'VOTES_DESC'
+}
