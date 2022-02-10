6ba410d2eda6c21f9043a788af15f63879be0ae1
Backend CI <ott.rnd.core@kaltura.com>
2022-02-10 15:59:15 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/785, branch 7_2_1

--- src/environment.ts
+++ src/environment.ts
@@ -12,7 +12,7 @@ export interface Environment {
 
 export const environment: Environment = {
     request: {
-        apiVersion: '7.2.0.29747',
+        apiVersion: '7.2.0.29748',
         ottMode: true,
         fileFormatValue: 20
     },
