8c3df3df6c2e6eaedb058f9e42282301ac2a8c46
Backend CI <ott.rnd.core@kaltura.com>
2023-03-07 10:24:45 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1074, branch BEO-13521_Segmentation_AssetUserRuleId

--- src/environment.ts
+++ src/environment.ts
@@ -12,7 +12,7 @@ export interface Environment {
 
 export const environment: Environment = {
     request: {
-        apiVersion: '8.5.1.30206',
+        apiVersion: '8.5.1.30283',
         ottMode: true,
         fileFormatValue: 20
     },
