09489e5859c29f94c06a970ddb37c88c24a1ecb1
Backend CI <ott.rnd.core@kaltura.com>
2023-06-18 13:36:22 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1139, branch 8.9-01

---
+++ src/api/types/KalturaLineupRegionalChannelOrderBy.ts
@@ -0,0 +1,8 @@
+
+
+export enum KalturaLineupRegionalChannelOrderBy {
+    lcnAsc = 'LCN_ASC',
+	lcnDesc = 'LCN_DESC',
+	nameAsc = 'NAME_ASC',
+	nameDesc = 'NAME_DESC'
+}
--- src/environment.ts
+++ src/environment.ts
@@ -12,7 +12,7 @@ export interface Environment {
 
 export const environment: Environment = {
     request: {
-        apiVersion: '8.6.12.30274',
+        apiVersion: '8.9.1.30368',
         ottMode: true,
         fileFormatValue: 20
     },
