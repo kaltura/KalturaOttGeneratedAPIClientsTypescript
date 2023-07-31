f57f00df7b179f4f5a1d0880fa16ae65c6ebd0b2
Backend CI <ott.rnd.core@kaltura.com>
2023-07-31 13:21:33 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1186, branch 9_1_0

--- src/api/types/KalturaVodIngestAssetResultResponse.ts
+++ src/api/types/KalturaVodIngestAssetResultResponse.ts
@@ -1,19 +1,19 @@
 
 import { KalturaObjectMetadata } from '../kaltura-object-base';
 import { KalturaTypesFactory } from '../kaltura-types-factory';
-import { KalturaVodIngestAssetResultListResponse } from './KalturaVodIngestAssetResultListResponse';
+import { KalturaVodIngestAssetResultList } from './KalturaVodIngestAssetResultList';
 import { KalturaVodIngestAssetResultAggregation } from './KalturaVodIngestAssetResultAggregation';
 import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
 
 export interface KalturaVodIngestAssetResultResponseArgs  extends KalturaObjectBaseArgs {
-    result? : KalturaVodIngestAssetResultListResponse;
+    result? : KalturaVodIngestAssetResultList;
 	aggregations? : KalturaVodIngestAssetResultAggregation;
 }
 
 
 export class KalturaVodIngestAssetResultResponse extends KalturaObjectBase {
 
-    result : KalturaVodIngestAssetResultListResponse;
+    result : KalturaVodIngestAssetResultList;
 	aggregations : KalturaVodIngestAssetResultAggregation;
 
     constructor(data? : KalturaVodIngestAssetResultResponseArgs)
@@ -28,7 +28,7 @@ export class KalturaVodIngestAssetResultResponse extends KalturaObjectBase {
             result.properties,
             {
                 objectType : { type : 'c', default : 'KalturaVodIngestAssetResultResponse' },
-				result : { type : 'o', subTypeConstructor : KalturaVodIngestAssetResultListResponse, subType : 'KalturaVodIngestAssetResultListResponse' },
+				result : { type : 'o', subTypeConstructor : KalturaVodIngestAssetResultList, subType : 'KalturaVodIngestAssetResultList' },
 				aggregations : { type : 'o', subTypeConstructor : KalturaVodIngestAssetResultAggregation, subType : 'KalturaVodIngestAssetResultAggregation' }
             }
         );
