6c5bc543f2d3cd0504ee96fb8a608ae266605845
Backend CI <ott.rnd.core@kaltura.com>
2025-03-03 11:08:27 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1430, branch 11.0.0.1

---
+++ src/api/types/KalturaSubtitles.ts
@@ -0,0 +1,45 @@
+
+import { KalturaObjectMetadata } from '../kaltura-object-base';
+import { KalturaTypesFactory } from '../kaltura-types-factory';
+import { KalturaSubtitlesType } from './KalturaSubtitlesType';
+import { KalturaObjectBase, KalturaObjectBaseArgs } from '../kaltura-object-base';
+
+export interface KalturaSubtitlesArgs  extends KalturaObjectBaseArgs {
+    fileName? : string;
+	type? : KalturaSubtitlesType;
+	language? : string;
+}
+
+
+export class KalturaSubtitles extends KalturaObjectBase {
+
+    readonly id : number;
+	readonly createDate : number;
+	fileName : string;
+	type : KalturaSubtitlesType;
+	language : string;
+
+    constructor(data? : KalturaSubtitlesArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaSubtitles' },
+				id : { type : 'n', readOnly : true },
+				createDate : { type : 'n', readOnly : true },
+				fileName : { type : 's' },
+				type : { type : 'es', subTypeConstructor : KalturaSubtitlesType, subType : 'KalturaSubtitlesType' },
+				language : { type : 's' }
+            }
+        );
+        return result;
+    }
+}
+
+KalturaTypesFactory.registerType('KalturaSubtitles',KalturaSubtitles);
