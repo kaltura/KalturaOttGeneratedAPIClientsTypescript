cd34595976799f3fd404a5c18d38cab913eecc18
Backend CI <ott.rnd.core@kaltura.com>
2025-03-20 12:30:52 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1446, branch BEO-16898_Users-activity

--- src/api/types/KalturaManualChannel.ts
+++ src/api/types/KalturaManualChannel.ts
@@ -5,13 +5,15 @@ import { KalturaManualCollectionAsset } from './KalturaManualCollectionAsset';
 import { KalturaChannel, KalturaChannelArgs } from './KalturaChannel';
 
 export interface KalturaManualChannelArgs  extends KalturaChannelArgs {
-    assets? : KalturaManualCollectionAsset[];
+    mediaIds? : string;
+	assets? : KalturaManualCollectionAsset[];
 }
 
 
 export class KalturaManualChannel extends KalturaChannel {
 
-    assets : KalturaManualCollectionAsset[];
+    mediaIds : string;
+	assets : KalturaManualCollectionAsset[];
 
     constructor(data? : KalturaManualChannelArgs)
     {
@@ -26,6 +28,7 @@ export class KalturaManualChannel extends KalturaChannel {
             result.properties,
             {
                 objectType : { type : 'c', default : 'KalturaManualChannel' },
+				mediaIds : { type : 's' },
 				assets : { type : 'a', subTypeConstructor : KalturaManualCollectionAsset, subType : 'KalturaManualCollectionAsset' }
             }
         );
