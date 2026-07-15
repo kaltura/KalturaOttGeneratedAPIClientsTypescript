3be3a37051646efab05c3a5896857bba723f6b27
Backend CI <ott.rnd.core@kaltura.com>
2026-07-15 09:33:56 +0000

    Auto-generated by Jenkins job OTT-Generate-ClientLibs/1630, branch padded_series_recording

---
+++ src/api/types/KalturaTextMetaConstraint.ts
@@ -0,0 +1,37 @@
+
+import { KalturaObjectMetadata } from '../kaltura-object-base';
+import { KalturaTypesFactory } from '../kaltura-types-factory';
+import { KalturaBaseAttributeConstraint, KalturaBaseAttributeConstraintArgs } from './KalturaBaseAttributeConstraint';
+
+export interface KalturaTextMetaConstraintArgs  extends KalturaBaseAttributeConstraintArgs {
+    contains? : string;
+	equals? : string;
+}
+
+
+export class KalturaTextMetaConstraint extends KalturaBaseAttributeConstraint {
+
+    contains : string;
+	equals : string;
+
+    constructor(data? : KalturaTextMetaConstraintArgs)
+    {
+        super(data);
+    }
+
+    protected _getMetadata() : KalturaObjectMetadata
+    {
+        const result = super._getMetadata();
+        Object.assign(
+            result.properties,
+            {
+                objectType : { type : 'c', default : 'KalturaTextMetaConstraint' },
+				contains : { type : 's' },
+				equals : { type : 's' }
+            }
+        );
+        return result;
+    }
+}
+
+KalturaTypesFactory.registerType('KalturaTextMetaConstraint',KalturaTextMetaConstraint);
